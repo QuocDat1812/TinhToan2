d17443fea3d3514be2a70d0012a126bf989ea074
TuanAnhhg <0903699@gmail.com>
2019-10-03 10:01:52 +0700

    Add project files.

---
+++ TongChuoi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.IO;
+
+namespace TongChuoi
+{
+    class Program
+    {
+         const string PROMPT = "# ";
+            public static void Main()
+            {
+                Console.WriteLine("Y2 String to Expression Tree");
+                Console.WriteLine("https://yinyangit.wordpress.com");
+
+                while (true)
+                {
+                    Console.Write(PROMPT);
+                    string input = Console.ReadLine().Trim();
+                    if (input == "")
+                    {
+                        Console.CursorTop--;
+                        continue;
+                    }
+
+                    if (string.Compare(input, "exit", true) == 0)
+                        break;
+
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    try
+                    {
+                        Scanner scanner = new Scanner(input);
+                        Parser parser = new Parser(scanner);
+                        Expression expr = parser.Parse();
+                        Delegate func = Expression.Lambda(expr).Compile();
+                        Console.WriteLine("  " + func.DynamicInvoke());
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    Console.ResetColor();
+                    Console.ReadLine();
+                }
+            }
+    }
+}
+
